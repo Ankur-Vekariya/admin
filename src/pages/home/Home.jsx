4bdad2fb44b2fd7655940a6923248a56ff072098
ankur12v <65348445+ankur12v@users.noreply.github.com>
2022-11-24 22:34:46 +0530

    commit 2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">
-    <h1 className="title">Ankur ekariya developer</h1>
-  </div>;
+  return (
+    <div className="home">
+      <h1 className="title">Ankur Vekariya Developer</h1>
+    </div>
+  );
 };
 
 export default Home;
