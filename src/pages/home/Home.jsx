450d2e3b5380dcaa49c2573e703df2754725348c
ankur12v <65348445+ankur12v@users.noreply.github.com>
2022-11-24 22:06:44 +0530

    installation

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./home.scss";
+
+const Home = () => {
+  return <div className="home">
+    <h1 className="title">Ankur vekariya developer</h1>
+  </div>;
+};
+
+export default Home;
